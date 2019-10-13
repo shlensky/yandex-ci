d4b73dc2200219e147167412d5b69e387fb2d8d8
Dmitry Shlensky <shld@list.ru>
2019-10-13 14:54:27 +0300

    Check if agent is already registered

--- server/index.js
+++ server/index.js
@@ -16,12 +16,22 @@ app.get('/', (req, res) => res.render('index', {}));
 app.get('/build/:id', (req, res) => res.render('build', {}));
 
 // API for agents
+const agents = [];
 app.post('/notify_agent', (req, res) => {
   // -- зарегистрировать агента . В параметрах хост и порт, на котором запущен агент
   const {host, port} = req.body;
   if (!host || !port) {
     throw new Error('Host and port required!');
   }
+
+  // Check if agent is already registered
+  if (agents.some((agent) => agent.host === host && agent.port === port)) {
+    console.info(`Agent is already registered, agent url is http://${host}:${port}`);
+    res.status(200).send('Agent is already registered');
+    return;
+  }
+
+  agents.push({host, port});
 
   console.info(`Agent registered, agent url is http://${host}:${port}`);
   res.status(204).end();
